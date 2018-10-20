e929a2d68cb294b59df92c83888585fe7adcf4d8
Rozhoň Ivan <irozhon@trask.cz>
2018-10-20 23:17:27 +0200

    add materialize + typings; add material icons; add content

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,8 +1,24 @@
-import { Component } from '@angular/core';
+import { Component, AfterViewInit, ViewChild, ElementRef } from '@angular/core';
+
+// import { FloatingActionButton } from 'materialize-css';
 
 @Component({
   selector: 'ngi-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {}
+export class AppComponent implements AfterViewInit {
+  imageUrl: string;
+
+  constructor() {
+    this.imageUrl = './../assets/images/rozhon.jpeg';
+  }
+
+  // @ViewChild('fixedActionBtn')
+  // fixedActionBtn: ElementRef;
+
+  ngAfterViewInit(): void {
+    // // initialize materialize components
+    // FloatingActionButton.init(this.fixedActionBtn.nativeElement);
+  }
+}
